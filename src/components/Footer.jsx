91302cdec4525254722ffd7a23c1c749656bbf45
PabloM <paanmuav@gmail.com>
2025-04-05 22:14:14 -0500

    Estilos en Mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="text-white text-center mt-10 px-4 pb-4 sm:pb-6">
+    <footer className="text-white text-center mt-10 px-4 pb-4 sm:pb-4">
       <p className="text-sm sm:text-base font-medium tracking-wide animate-fade-in">
         {getMessage()}
       </p>
